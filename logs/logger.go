14cbeb674979a396b001d23340dbbcdf609e1f4e
ellexo2456 <92405774+ellexo2456@users.noreply.github.com>
2023-10-02 23:14:55 +0300

    Update logger.go

--- logs/logger.go
+++ logs/logger.go
@@ -12,10 +12,10 @@ func LoggerInit() *logrus.Logger {
 	logger := logrus.New()
 	logger.SetFormatter(&logrus.TextFormatter{})
 
-	//err := godotenv.Load()
-	//if err != nil {
-	//	logger.Fatal("Failed to get config : ", err)
-	//}
+	err := godotenv.Load("../.env")
+	if err != nil {
+		logger.Fatal("Failed to get config : ", err)
+	}
 
 	logLevel := os.Getenv("LOG_LEVEL")
 	switch logLevel {
