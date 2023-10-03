4b2711acfed91acf7bae592f4b59da836bef519f
Alexey <ax.chinaev@yandex.ru>
2023-10-03 18:25:26 +0300

    usecase tests

--- auth/usecase/auth_test.go
+++ auth/usecase/auth_test.go
@@ -138,7 +138,6 @@ func TestLogout(t *testing.T) {
 			setSessionRepoExpectations: func(sessionRepo *mocks.SessionRepository) {
 				sessionRepo.On("DeleteByToken", mock.Anything).Return(errors.New("some db error")).Maybe()
 			},
-			good: false,
 		},
 		{
 			name:  "BadCase/InvalidToken",
@@ -146,7 +145,6 @@ func TestLogout(t *testing.T) {
 			setSessionRepoExpectations: func(sessionRepo *mocks.SessionRepository) {
 				sessionRepo.On("DeleteByToken", mock.Anything).Return(errors.New("another db error"))
 			},
-			good: false,
 		},
 	}
 
@@ -174,18 +172,66 @@ func TestLogout(t *testing.T) {
 
 func TestRegister(t *testing.T) {
 	tests := []struct {
-		name                       string
-		token                      string
-		setUserUsecaseExpectations func(u *authUsecase)
-		good                       bool
+		name                        string
+		id                          int
+		getUser                     func() domain.User
+		setUserAuthRepoExpectations func(ar *mocks.AuthRepository, id int)
+		good                        bool
 	}{
 		{
-			name:  "GoodCase/Common",
-			token: uuid.NewString(),
-			setUserUsecaseExpectations: func(u *authUsecase) {
-				sessionRepo.On("DeleteByToken", mock.Anything).Return(nil)
+			name: "GoodCase/Common",
+			getUser: func() domain.User {
+				var user domain.User
+				faker.FakeData(&user)
+				user.Email = "uvybini@mail.ru"
+				return user
+			},
+			id: 10,
+			setUserAuthRepoExpectations: func(ar *mocks.AuthRepository, id int) {
+				ar.On("UserExists", mock.Anything).Return(false, nil)
+				ar.On("AddUser", mock.Anything).Return(id, nil)
 			},
 			good: true,
+		},
+		{
+			name: "BadCase/EmptyUser",
+			getUser: func() domain.User {
+				return domain.User{}
+			},
+			id: 0,
+			setUserAuthRepoExpectations: func(ar *mocks.AuthRepository, id int) {
+				ar.On("UserExists", mock.Anything).Return(true, nil).Maybe()
+				ar.On("AddUser", mock.Anything).Return(0, errors.New("some db error"))
+			},
+		},
+		{
+			name: "BadCase/EmptyCreds",
+			getUser: func() domain.User {
+				var user domain.User
+				faker.FakeData(&user)
+				user.Email = "dfsfs@mail.ru"
+				return user
+			},
+			id: 0,
+			setUserAuthRepoExpectations: func(ar *mocks.AuthRepository, id int) {
+				ar.On("UserExists", mock.Anything).Return(true, nil)
+				ar.On("AddUser", mock.Anything).Return(0, errors.New("some db error")).Maybe()
+			},
+		},
+		{
+			name: "BadCase/AlreadyCreated",
+			getUser: func() domain.User {
+				var user domain.User
+				faker.FakeData(&user)
+				user.Email = ""
+				user.Password = ""
+				return user
+			},
+			id: 0,
+			setUserAuthRepoExpectations: func(ar *mocks.AuthRepository, id int) {
+				ar.On("UserExists", mock.Anything).Return(false, errors.New("some db error"))
+				ar.On("AddUser", mock.Anything).Return(0, errors.New("some db error"))
+			},
 		},
 	}
 
@@ -195,18 +241,78 @@ func TestRegister(t *testing.T) {
 
 			ar := new(mocks.AuthRepository)
 			sr := new(mocks.SessionRepository)
-			test.setSessionRepoExpectations(sr)
+			test.setUserAuthRepoExpectations(ar, test.id)
 
 			auCase := usecase.NewAuthUsecase(ar, sr)
-			err := auCase.Logout(test.token)
+			id, err := auCase.Register(test.getUser())
 
 			if test.good {
 				assert.Nil(t, err)
+				assert.Equal(t, id, test.id)
 			} else {
 				assert.NotNil(t, err)
+				assert.Equal(t, id, 0)
 			}
 
-			sr.AssertExpectations(t)
+			ar.AssertExpectations(t)
+		})
+	}
+}
+
+func TestIsAuth(t *testing.T) {
+	tests := []struct {
+		name                       string
+		token                      string
+		setSessionRepoExpectations func(sessionRepo *mocks.SessionRepository, auth bool)
+		good                       bool
+		auth                       bool
+	}{
+		{
+			name:  "GoodCase/Common",
+			token: uuid.NewString(),
+			setSessionRepoExpectations: func(sessionRepo *mocks.SessionRepository, auth bool) {
+				sessionRepo.On("SessionExists", mock.Anything).Return(auth, nil)
+			},
+			good: true,
+			auth: true,
+		},
+		{
+			name:  "BadCase/EmptyToken",
+			token: "",
+			setSessionRepoExpectations: func(sessionRepo *mocks.SessionRepository, auth bool) {
+				sessionRepo.On("SessionExists", mock.Anything).Return(auth, errors.New("some db error")).Maybe()
+			},
+		},
+		{
+			name:  "BadCase/InvalidToken",
+			token: "8/refvd 3fdf  sdc",
+			setSessionRepoExpectations: func(sessionRepo *mocks.SessionRepository, auth bool) {
+				sessionRepo.On("SessionExists", mock.Anything).Return(auth, errors.New("some db error"))
+			},
+		},
+	}
+
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			//t.Parallel()
+
+			ar := new(mocks.AuthRepository)
+			sr := new(mocks.SessionRepository)
+			test.setSessionRepoExpectations(sr, test.auth)
+
+			auCase := usecase.NewAuthUsecase(ar, sr)
+			auth, err := auCase.IsAuth(test.token)
+
+			if test.good {
+				assert.Nil(t, err)
+				assert.Equal(t, auth, test.auth)
+				assert.True(t, auth)
+			} else {
+				assert.NotNil(t, err)
+				assert.False(t, auth)
+			}
+
+			ar.AssertExpectations(t)
 		})
 	}
 }
