66a68b9bfb3b61b8ab3524a947273dde6885b749
Alexey <ax.chinaev@yandex.ru>
2023-10-03 16:47:59 +0300

    remove csp

--- auth/delivery/http/middleware/middleware.go
+++ auth/delivery/http/middleware/middleware.go
@@ -22,7 +22,6 @@ func (m *Middleware) CORS(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("Access-Control-Allow-Origin", "http://localhost")
 		w.Header().Set("Access-Control-Allow-Credentials", "true")
-		w.Header().Set("Content-Security-Policy", "default-src 'self' http://localhost")
 
 		if r.Method == http.MethodOptions {
 			w.Header().Set("Access-Control-Allow-Headers", "Content-Type, User-Agent")
