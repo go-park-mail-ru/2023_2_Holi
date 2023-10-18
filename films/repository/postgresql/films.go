4971ea6f5eda7883abebd33c3dfaab5dcb5dbc68
ShelbyKS <krylov.sanches@mail.ru>
2023-10-18 18:25:23 +0300

    fixed film repo

--- films/repository/postgresql/films.go
+++ films/repository/postgresql/films.go
@@ -17,11 +17,7 @@ func NewFilmsPostgresqlRepository(conn *sql.DB) domain.FilmsRepository {
 
 func (r *filmsPostgresqlRepository) GetFilmsByGenre(genre string) ([]domain.Film, error) {
 	rows, err := r.db.Query(
-		`SELECT film.id, film.name, film.preview_path, film.rating
-		FROM film
-		JOIN "genre_film" gf ON film.id = gf.film_id
-		JOIN genre g ON gf.genre_id = g.id
-		WHERE g.name = $1`, genre)
+		`SELECT film.id, film.name, film.preview_path, film.rating FROM film JOIN "genre_film" gf ON film.id = gf.film_id JOIN genre g ON gf.genre_id = g.id WHERE g.name = $1`, genre)
 	if err != nil {
 		logs.LogError(logs.Logger, "films_postgresql", "GetFilmsByGenre", err, err.Error())
 		return nil, err
@@ -95,9 +91,9 @@ func (r *filmsPostgresqlRepository) GetFilmData(id int) (*domain.Film, error) {
 
 func (r *filmsPostgresqlRepository) GetFilmArtists(FilmId int) ([]domain.Artist, error) {
 	rows, err := r.db.Query(
-		`SELECT artist_id
-		FROM artist-film
-		WHERE film_id = $1`, FilmId)
+		`SELECT name FROM "artist" JOIN "artist-film" af 
+		ON id = artist_id 
+		WHERE af.film_id = $1`, FilmId)
 	if err != nil {
 		return nil, err
 	}
@@ -110,51 +106,16 @@ func (r *filmsPostgresqlRepository) GetFilmArtists(FilmId int) ([]domain.Artist,
 
 	var artists []domain.Artist
 	for rows.Next() {
-		var artistID int
+		var artist domain.Artist
 		err = rows.Scan(
-			&artistID,
+			&artist.Name,
 		)
 		if err != nil {
 			logs.LogError(logs.Logger, "films_postgresql", "GetFilmArtists", err, err.Error())
 			return nil, err
 		}
-		artist, err := r.getArtistDataByID(artistID)
-		if err != nil {
-			logs.LogError(logs.Logger, "films_postgresql", "GetFilmArtists", err, err.Error())
-			return nil, err
-		}
-		artists = append(artists, *artist)
+		artists = append(artists, artist)
 	}
 
 	return artists, nil
-}
-
-func (r *filmsPostgresqlRepository) getArtistDataByID(id int) (*domain.Artist, error) {
-	row, err := r.db.Query(
-		`SELECT name
-		FROM artist
-		WHERE id = $1`, id)
-	if err != nil {
-		logs.LogError(logs.Logger, "films_postgresql", "getArtistDataByID", err, err.Error())
-		return nil, err
-	}
-	defer func(row *sql.Rows) {
-		err := row.Close()
-		if err != nil {
-			logs.LogError(logs.Logger, "films_postgresql", "getArtistDataByID", err, err.Error())
-		}
-	}(row)
-
-	artist := new(domain.Artist)
-	err = row.Scan(
-		&artist.ID,
-		&artist.Name,
-	)
-
-	if err != nil {
-		logs.LogError(logs.Logger, "films_postgresql", "getArtistDataByID", err, err.Error())
-		return nil, err
-	}
-
-	return artist, nil
 }
