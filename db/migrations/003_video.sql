dc2f46adba34b7143f66584ead1d582ad8c8c3b9
Alexey <ax.chinaev@yandex.ru>
2023-10-29 22:40:22 +0300

    fixed video tabel

--- db/migrations/003_video.sql
+++ db/migrations/003_video.sql
@@ -1,22 +1,23 @@
 CREATE TABLE video
 (
-    id              SERIAL PRIMARY KEY,
-    name            TEXT                  NOT NULL,
-    description     TEXT,
-    preview_path    TEXT                  NOT NULL,
-    release_year    INTEGER
-                    CONSTRAINT release_year_range
-                    CHECK (release_year >= 1890
-                    AND release_year <= EXTRACT(YEAR FROM CURRENT_DATE)),
-    rating          FLOAT(2)
-                    CONSTRAINT rating_range
-                    CHECK (rating BETWEEN 0 AND 10),
-    age_restriction INTEGER
-                    CONSTRAINT age_restriction_range
-                    CHECK (rating BETWEEN 0 AND 100),
-    seasons_count   INT         DEFAULT 0 NOT NULL,
-    created_at      TIMESTAMPTZ DEFAULT CURRENT_TIMESTAMP,
-    updated_at      TIMESTAMPTZ DEFAULT CURRENT_TIMESTAMP
+    id                 SERIAL PRIMARY KEY,
+    name               TEXT,
+    description        TEXT,
+    preview_path       TEXT,
+    preview_video_path TEXT                  NOT NULL,
+    release_year       INTEGER
+                       CONSTRAINT release_year_range
+                       CHECK (release_year >= 1890
+                       AND release_year <= EXTRACT(YEAR FROM CURRENT_DATE)),
+    rating             FLOAT(2)
+                       CONSTRAINT rating_range
+                       CHECK (rating BETWEEN 0 AND 10),
+    age_restriction    INTEGER
+                       CONSTRAINT age_restriction_range
+                       CHECK (rating BETWEEN 0 AND 100),
+    seasons_count      INT         DEFAULT 0 NOT NULL,
+    created_at         TIMESTAMPTZ DEFAULT CURRENT_TIMESTAMP,
+    updated_at         TIMESTAMPTZ DEFAULT CURRENT_TIMESTAMP
 );
 
 CREATE TRIGGER modify_video_updated_at
