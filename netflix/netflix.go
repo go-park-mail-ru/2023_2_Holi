08fb16e8ec1250f8a808c92a0121451a4200cb14
ShelbyKS <krylov.sanches@mail.ru>
2023-11-26 19:18:27 +0300

    search tests

--- netflix/netflix.go
+++ netflix/netflix.go
@@ -4,18 +4,20 @@ import (
 	g_sess "2023_2_Holi/domain/grpc/session"
 	"context"
 	"embed"
+	"net/http"
+	"os"
+
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/aws/session"
 	"github.com/aws/aws-sdk-go/service/s3"
 	"github.com/microcosm-cc/bluemonday"
-	"net/http"
-	"os"
 
 	"github.com/gorilla/mux"
 
 	films_http "2023_2_Holi/films/delivery/http"
 	films_postgres "2023_2_Holi/films/repository/postgresql"
 	films_usecase "2023_2_Holi/films/usecase"
+
 	"github.com/joho/godotenv"
 
 	genre_http "2023_2_Holi/genre/delivery/http"
@@ -31,6 +33,7 @@ import (
 	"2023_2_Holi/connectors/postgres"
 	logs "2023_2_Holi/logger"
 	"2023_2_Holi/middleware"
+
 	_ "github.com/lib/pq"
 )
 
