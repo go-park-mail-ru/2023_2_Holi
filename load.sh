d8ed937367d69829f7c1910f28236892bee21065
Alexey <ax.chinaev@yandex.ru>
2023-11-26 14:45:52 +0300

    add metrics staff

--- load.sh
+++ load.sh
@@ -1,4 +1,11 @@
-hey -z 5m -q 200 -m POST http://localhost:3001/api/v1/auth/register &&
-hey -z 5m -q 400 -m POST http://localhost:3001/api/v1/auth/login &&
-hey -z 5m -q 200 -m POST http://localhost:3001/api/v1/auth/logout &&
-hey -z 5m -q 200 -m POST http://localhost:3001/api/v1/auth/check
+time=30m
+
+reg="hey -z $time -q 20 -m POST http://localhost:3001/api/v1/auth/register"
+login="hey -z $time -q 100 -m POST http://localhost:3001/api/v1/auth/login"
+logout="hey -z $time -q 15 -m POST http://localhost:3001/api/v1/auth/logout"
+check="hey -z $time -q 30 -m POST http://localhost:3001/api/v1/auth/check"
+
+osascript -e "tell app \"Terminal\" to do script \"$reg\""
+osascript -e "tell app \"Terminal\" to do script \"$login\""
+osascript -e "tell app \"Terminal\" to do script \"$logout\""
+osascript -e "tell app \"Terminal\" to do script \"$check\""
