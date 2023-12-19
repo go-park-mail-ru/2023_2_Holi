b05b0962e4a2870e89356796b18674a6473531b1
Alexey <ax.chinaev@yandex.ru>
2023-12-19 20:55:58 +0300

    add trigger

--- ansible/files/init.sql
+++ ansible/files/init.sql
@@ -93,6 +93,27 @@ CREATE TRIGGER modify_video_estimation_updated_at
     FOR EACH ROW
 EXECUTE PROCEDURE public.moddatetime(updated_at);
 
+CREATE FUNCTION update_video_rating() RETURNS TRIGGER
+    LANGUAGE plpgsql AS $$
+BEGIN
+UPDATE video v
+SET rating = COALESCE(CAST((CAST(ve.rating_sum AS NUMERIC(20, 1)) / ve.rating_count) AS NUMERIC(20, 1)), 0)
+    FROM (SELECT COUNT(*) AS rating_count, SUM(rate) AS rating_sum
+          FROM video_estimation
+          WHERE video_id = COALESCE(NEW.video_id, OLD.video_id)) ve
+WHERE v.id = COALESCE(NEW.video_id, OLD.video_id);
+
+RETURN NEW;
+END;
+$$;
+
+
+CREATE OR REPLACE TRIGGER insert_update_delete_vide_estimation_rate
+    AFTER INSERT OR UPDATE OF rate OR DELETE
+                    ON video_estimation
+                        FOR EACH ROW
+                        EXECUTE FUNCTION update_video_rating();
+
 CREATE TABLE genre
 (
     id   serial PRIMARY KEY,
