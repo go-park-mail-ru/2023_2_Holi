6601a39aefbe92f7683d4ca3dfb8d211ea1c0aba
Alexey <ax.chinaev@yandex.ru>
2023-12-20 17:57:01 +0300

    hotfix

--- coverage_check.sh
+++ coverage_check.sh
@@ -1,10 +1,9 @@
 #go test -coverprofile=coverage.out.tmp ./...
 #grep -v "domain/mocks" coverage.out.tmp > coverage.out
 #go test -coverprofile=coverage.out.tmp -coverpkg=./...  ./...
-#cat coverage.out.tmp | grep -v _mock.go | grep -v _easyjson.go | grep -v .pb.go | grep -v _grpc.go | grep -v domain/mocks/ > coverage.out
+#cat coverage.out.tmp | grep -v _mock.go | grep -v _easyjson.go | grep -v .pb.go | grep -v _grpc.go | grep -v domain > coverage.out
 #go tool cover -func=coverage.out
 #go tool cover --html=coverage.out
-#user
 
 go test -coverprofile=coverage.out.tmp ./...
 grep -v "domain" coverage.out.tmp > coverage.out
